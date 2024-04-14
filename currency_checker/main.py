3bd5771a3b97443ac78e512ef952804d2d8eca01
DawidDziekan <dziekan.dawid@wp.pl>
2024-04-14 15:41:56 +0200

    Update main.py

--- currency_checker/main.py
+++ currency_checker/main.py
@@ -2,7 +2,7 @@ import aiohttp
 import asyncio
 import datetime
 import platform
-
+import sys
 
 class ExchangeRateFetcher:
     async def fetch(self, date):
@@ -19,6 +19,14 @@ class ExchangeRateFetcher:
             except aiohttp.ClientConnectorError as err:
                 print(f'Connection error: {url}', str(err))
 
+class DaysChecker:
+    def check(self, days):
+        if days >= 1 and days <= 10:
+            return True
+        else:
+            print("Input must be a number of days between 1 and 10.")
+            exit
+            
 async def get_exchange_rates_for_last_n_days(fetcher, n):
     current_date = datetime.date.today()
     exchange_rates = []
@@ -30,22 +38,26 @@ async def get_exchange_rates_for_last_n_days(fetcher, n):
             exchange_rates.append({formatted_date: rates})
     return exchange_rates
 
-async def main():
-    try:
-        days = int(input("Enter number of days: "))
-        if days < 1 or days > 10:
-            raise ValueError("Number of days must be between 1 and 10.")
-    except ValueError as e:
-        print("Invalid input. Please enter a number between 1 and 10.")
-        return
-
+async def main(days):
     fetcher = ExchangeRateFetcher()
     exchange_rates = await get_exchange_rates_for_last_n_days(fetcher, days)
     
     for item in exchange_rates:
         print(item)
-
+        
 if __name__ == "__main__":
     if platform.system() == 'Windows':
         asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
-    asyncio.run(main())
+        
+    checker = DaysChecker()
+    if len(sys.argv) == 2:
+        try:
+            number_of_days = int(sys.argv[1])
+            if checker.check(number_of_days):
+                asyncio.run(main(number_of_days))
+        except ValueError:
+            print("Input must be a number of days between 1 and 10.")
+            exit
+    else:
+        print("Input must be a number of days between 1 and 10.")
+        sys.exit(1)
